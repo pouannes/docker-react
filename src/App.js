8ca39b760fe74f9751bf2886ff6c20c8cfd9ebe1
pouannes <pierreouannes@gmail.com>
2020-07-14 08:52:14 +0100

    change welcoming message of react app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Here's how to continue: edit <code>src/App.js</code> and
+          Hey there! Here's how to continue: edit <code>src/App.js</code> and
           save to reload!
         </p>
         <a
